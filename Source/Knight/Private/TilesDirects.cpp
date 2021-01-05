c5811975de74bea1cdc31fad79873deec1643d66
ellementul <ananev.ivan.ur@ya.ru>
2021-01-05 15:23:21 +0500

    TileDirects was created

---
+++ Source/Knight/Private/TilesDirects.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TilesDirects.h"
+
+TilesDirects::TilesDirects()
+{
+}
+
+TilesDirects::~TilesDirects()
+{
+}
---
+++ Source/Knight/TileDirects.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TileDirects.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KNIGHT_API UTileDirects : public UObject
+{
+	GENERATED_BODY()
+	
+};
