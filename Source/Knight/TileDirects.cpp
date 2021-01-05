3fbecf3f87f4f1eca60b1c3943ae3b589522eca4
ellementul <ananev.ivan.ur@ya.ru>
2021-01-05 15:24:59 +0500

    TileDirects was correctly created

--- Source/Knight/TileDirects.cpp
+++ Source/Knight/TileDirects.cpp
@@ -3,3 +3,67 @@
 
 #include "TileDirects.h"
 
+UTileDirects::UTileDirects(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	Dirs.Add(0, FIntPoint(1, 0));
+	Dirs.Add(1, FIntPoint(0, 1));
+	Dirs.Add(2, FIntPoint(-1, 0));
+	Dirs.Add(3, FIntPoint(0, -1));
+}
+
+const TArray<int> UTileDirects::GetDirs()
+{
+	TArray<int>  IntDirs;
+	Dirs.GenerateKeyArray(IntDirs);
+	return IntDirs;
+}
+
+int UTileDirects::GetDirByCoords(FIntPoint Coords)
+{
+	const int* KeyPtr = Dirs.FindKey(Coords);
+	if (KeyPtr == nullptr) 
+	{
+		UE_LOG(LogTemp, Error, TEXT("UTileDirects::GetDirByCoords: Coords[%d,%d] is wrong!"), Coords.X, Coords.Y);
+		return 0;
+	}
+
+	return *KeyPtr;
+}
+
+FIntPoint UTileDirects::GetCoords(int Dir)
+{
+	if (!Dirs.Contains(Dir))
+	{
+		UE_LOG(LogTemp, Error, TEXT("UTileDirects::GetCoords: Dirs[%d] is wrong!"), Dir);
+		return Dirs[0];
+	}
+
+	return Dirs[Dir];
+}
+
+int UTileDirects::Invert(int Dir)
+{
+	int InvertDir = 0;
+	switch (Dir)
+	{
+	case 0:  InvertDir = 2;
+		break;
+	case 1:  InvertDir = 3;
+		break;
+	case 2:  InvertDir = 0;
+		break;
+	case 3:  InvertDir = 1;
+		break;
+	default:
+		UE_LOG(LogTemp, Error, TEXT(" UTileDirects::Invert: Dir[%d] is wrong!"), Dir);
+	}
+
+	return InvertDir;
+}
+
+FIntPoint UTileDirects::Invert(FIntPoint Coords)
+{
+	return GetCoords( Invert(GetDirByCoords(Coords)) );
+}
+
+
--- Source/Knight/TileDirects.h
+++ Source/Knight/TileDirects.h
@@ -6,12 +6,34 @@
 #include "UObject/NoExportTypes.h"
 #include "TileDirects.generated.h"
 
-/**
- * 
- */
+/*
+* Этоn класс определяет направления для тайла
+* Направления:
+* 0 - положительная полуось X
+* 1 - положительная полуось Y
+* 2 - отрицательная полуось X
+* 3 - отрицательная полуось Y
+*/
 UCLASS()
 class KNIGHT_API UTileDirects : public UObject
 {
 	GENERATED_BODY()
+
+public:
+
+	UTileDirects(const FObjectInitializer& ObjectInitializer);
+
+	const TArray<int> GetDirs();
+
+	int GetDirByCoords(FIntPoint Coords);
+
+	FIntPoint GetCoords(int Dir);
+
+	FIntPoint Invert(FIntPoint Coords);
+
+	int       Invert(int Dir);
+
+private:
+	TMap<int, FIntPoint> Dirs;
 	
 };
